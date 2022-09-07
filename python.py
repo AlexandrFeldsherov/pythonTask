4e2369733731765527d9c28884e8b0ec83f1f8ca
[AlexandrFeldsherov] <[alexandrfeldsherov@mail.ru]>
2022-09-07 17:49:20 +0300

    v002

--- python.py
+++ python.py
@@ -1,6 +1,23 @@
-x = int(input('введите первое число: '))
-y = int(input('введите второе число: '))
-if x**2 == y or y**2 == x:
-    print('условие выполнено')
-else:
-    print('условие выполнено')
+from random import randint
+
+# def is_number_power(x,y)
+#    if x**2 == y or y**2 == x:
+#        print('условие выполнено')
+#    else:
+#       print('условие выполнено')
+
+# x = int(input('введите первое число: '))
+# y = int(input('введите второе число: '))
+# is_number_power(x,y)
+
+# def array_max(array):
+#     max=array[0]
+#     for i in array:
+#         if i>max:
+#             max=i
+#     return max
+        
+# array=[randint(1,100) for i in range(5)]
+# print(f"{array} -> {array_max(array)}")
+
+
